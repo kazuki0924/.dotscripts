8131ae8687dd06c10b987f8f96e4aac3705bb239
Kazuki M <kazuki0924@gmail.com>
2021-11-18 01:24:22 +0900

    Update: brew

--- setup/setup_brew.sh
+++ setup/setup_brew.sh
@@ -5,13 +5,17 @@ IFS=$'\n\t'
 # install Homebrew
 
 if ! which brew &>/dev/null; then
-  sudo -v
-  echo | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-fi
-
-if which brew &>/dev/null; then
-  set +e
-  brew doctor
-  set -e
-  brew update --verbose
+	sudo -v
+	echo | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+	brew tap homebrew/bundle
+	brew tap homebrew/cask
+	brew tap homebrew/cask-drivers
+	brew tap homebrew/cask-fonts
+	brew tap homebrew/cask-versions
+	brew tap homebrew/core
+	brew tap homebrew/services
+	set +e
+	brew doctor
+	set -e
+	brew update --verbose
 fi
