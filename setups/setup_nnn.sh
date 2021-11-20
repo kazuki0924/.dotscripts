1173602d27a94e17314cdb512a6bb1faa5c19847
Kazuki M <kazuki0924@gmail.com>
2021-11-20 16:41:17 +0900

    Update: checkout to a commint for nnn

--- setups/setup_nnn.sh
+++ setups/setup_nnn.sh
@@ -8,12 +8,14 @@ IFS=$'\n\t'
 # Nerd Font installed
 
 # install nnn
+[[ -d "${HOME}/.oss/nnn" ]] && trash-put "${HOME}/.oss/nnn"
 [[ ! -d "${HOME}/.oss/nnn" ]] && git clone https://github.com/jarun/nnn "${HOME}/.oss/nnn"
 cd "$HOME"/.oss/nnn
+# git checkout tags/v4.3
+# Close previewer when opening file (#1171)
+git checkout 532532704e72156aff140a07f8d7768d596eec02
 make O_NERD=1
 [[ -f /usr/local/bin/nnn ]] && trash-put /usr/local/bin/nnn
 cp "${HOME}/.oss/nnn/nnn" /usr/local/bin/nnn
-
-# install nnn plugins:
-# https://github.com/jarun/nnn
-curl -sL https://raw.githubusercontent.com/jarun/nnn/master/plugins/getplugs | bash
+[[ -d "${HOME}/.config/nnn/plugins" ]] && trash-put "${HOME}/.config/nnn/plugins"
+cp -r "${HOME}/.oss/nnn/plugins" "${HOME}/.config/nnn"
