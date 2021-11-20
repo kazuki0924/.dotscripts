7cb860b456eb5832db252836370b321751553465
Kazuki M <kazuki0924@gmail.com>
2021-11-20 19:45:49 +0900

    Update: mysides

---
+++ macos/macos_mysides.sh
@@ -0,0 +1,51 @@
+#!/usr/local/bin/bash
+set -euo pipefail
+
+# Sidebar
+
+# Requirements:
+# mysides
+
+REQUIREMENTS=(
+	mysides
+)
+
+for REQUIREMENT in "${REQUIREMENTS[@]}"; do
+	which "${REQUIREMENT}" &>/dev/null || brew install "${REQUIREMENT}"
+done
+
+SIDEBAR_ITEMS_TO_DELETE=(
+	Recents
+	Documents
+	Desktop
+	Movies
+	Music
+	Pictures
+	/
+)
+
+for ITEM in "${SIDEBAR_ITEMS_TO_DELETE[@]}"; do
+  set +e
+	mysides remove "${ITEM}"  &>/dev/null
+  set -e
+	echo removed "${ITEM}" with mysides
+done
+
+SIDEBAR_ITEMS_TO_ADD=(
+	Repositories
+	Screenshots
+	Miscellaneous
+  .config
+	.dotfiles
+	.dotscripts
+  .zshfiles
+  .nvimfiles
+  .vscodefiles
+  .golandfiles
+)
+
+for ITEM in "${SIDEBAR_ITEMS_TO_ADD[@]}"; do
+	[[ ! -d ${ITEM} ]] && mkdir -pv "${HOME}/${ITEM}"
+	mysides add "${ITEM}" "file:///${HOME}/$ITEM" &>/dev/null
+	echo addded "${ITEM}" with mysides
+done
