ccfae17afc2e11489302066d27fdf9af052d3e61
Kazuki M <kazuki0924@gmail.com>
2021-11-22 05:30:47 +0900

    Add: mkdir

---
+++ setups/macos_mkdir_xcode-select.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+IFS=$'\n\t'
+
+# create directories
+DIRS=(
+	Repositories/{Personal,Work}
+	Screenshots/{Personal,Work}
+	Miscellaneous/{Personal,Work}
+	Documents/{Personal,Work}
+	.oss
+)
+
+for DIR in "${DIRS[@]}"; do
+	[ ! -d "$HOME/$DIR" ] && mkdir -p "$HOME/$DIR"
+	echo created directory: "$DIR"
+done
+
+# Command Line Tools
+xcode-select --install
