8ab5928a05e4c26bb9ea6ae2b911ffd83968adfd
Kazuki M <kazuki0924@gmail.com>
2021-12-20 12:31:17 +0900

    fix

--- watcher/watch_bash_repl.sh
+++ watcher/watch_bash_repl.sh
@@ -5,12 +5,12 @@ IFS=$'\n\t'
 declare -r CMD="clear && printf '\e[3J' && bash"
 [[ -n $* ]] && ARGS=$* || ARGS="repl.sh"
 
-[[ ! -d "${HOME}\scratches" ]] && mkdir "${HOME}\scratches" 
-[[ ! -f "${HOME}\scratches\repl.sh" ]] && touch "${HOME}\scratches\repl.sh" 
+[[ ! -d "${HOME}\scratches" ]] && mkdir "${HOME}/scratches" 
+[[ ! -f "${HOME}\scratches\repl.sh" ]] && touch "${HOME}/scratches/repl.sh" 
 
 eval "${CMD} ${ARGS}"
 watchman-make \
-  -r "${HOME}\scratches" \
+  -r "${HOME}/scratches" \
 	-p 'repl.sh' \
 	--make="${CMD}" \
 	-t "${ARGS}"
